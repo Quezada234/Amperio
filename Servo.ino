24af41d52e87c6373a2ab151a233bbb6d6255c07
Quezada234 <56000852+Quezada234@users.noreply.github.com>
2019-09-30 17:16:30 -0500

    Lógica Kaizen
    Practica para mejorar la capacidad en Hacer mejores proyectos en Hardware y SoftWare.

---
+++ Servo.ino
@@ -0,0 +1,28 @@
+*/SerovoMotor*/
+
+#include Servo.h
+
+Servo servoMotor;
+
+void setup(){
+
+Serial.begin(9600);
+
+servoMotor.attach(9);
+
+}
+
+void loop(){
+
+
+servoMotor.write(0);
+delay(1000);
+
+servoMotor.write(90);
+delay(2000);
+
+servoMotor.write(180);
+delay(3000);
+
+
+}
